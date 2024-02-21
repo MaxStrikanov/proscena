52e82c3ffb545d53cccebbd76c75161d536bed81
MaxStrikanov <maxvell.88@mail.ru>
2024-02-21 15:52:38 +0300

    Отзывы клиентов

--- main.js
+++ main.js
@@ -17,4 +17,24 @@ tl.to(".animation_1", {duration: 0.1, opacity: 1, y: -20 })
 
 
   
+  const API_URL = 'https://api.unisender.com/ru/api/subscribe?';
+  const API_KEY = '6x6q7z7desosw7j18upmyuxetyoshy8d378jr9qy';
+  const LIST_IDS = '19862270';
+  
+  const arr = [ 'velery-shoy@mail.ru', 'ramai-zetta@mail.ru', 'pp274662@gmail.com', 'kropotinchik@rambler.ru', 't.abramova@udm.ru', 'pozd80@mail.ru', 'frankinshtein@mail.ru']
+  
+  // Массив данных для отправки на сервер
+  // const url = `${API_URL}format=json&api_key=${API_KEY}&list_ids=${LIST_IDS}&fields[email]=${arrEmail}&double_optin=0&overwrite=0}`
 
+  
+  const arrEmail = []
+  var myString = ''
+  arr.forEach((item, i) => {
+    
+    let query = `data[${i}][0]=${item}&data[${i}]][1]=778899&`;
+    arrEmail.push(query)
+    myString = arrEmail.join('').slice(0, -1);
+  })
+
+    console.log(myString);
+ 
